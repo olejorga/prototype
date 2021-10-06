abf47b036239bdb9253585ebec06bb84e30c8810
eriktja <erik.jarem@gmail.com>
2021-10-06 10:49:49 +0200

    create item_func.py
    made item_func.py in components package.
    
    This file will contain functions for items

--- src/models/item.py
+++ src/models/item.py
@@ -1,20 +1,7 @@
 from pydantic import BaseModel
-from datetime import datetime, timedelta
+from datetime import datetime
 
-def countdown(auction_date: datetime):
-    current_date = datetime.now()
-
-    count = int((auction_date - current_date).total_seconds)
-
-    days_remaining = count // 86400
-    hours_remaining = (count-days_remaining * 86400) // 3600
-    minutes_remaining = (count - days_remaining * 86400 - hours_remaining * 3600) // 60
-    seconds_remaining = count - days_remaining * 86400 - hours_remaining * 3600 - minutes_remaining * 60
-
-    string_output = ("{} days, {} hours, {} minutes and {} seconds left of the auction".format(days_remaining, hours_remaining, minutes_remaining, seconds_remaining))
     
-    return string_output
-      
 
 class Item(BaseModel):
     name: str = ""
