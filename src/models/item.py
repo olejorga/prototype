bae149f38d03a0947cb914d0e6be75f52d5071d9
olejorga <olejorga@icloud.com>
2021-10-15 09:46:02 +0200

    [FIX]: Endret py versjon og list type hint

--- src/models/item.py
+++ src/models/item.py
@@ -6,6 +6,6 @@ class Item(BaseModel):
     name: str = ""
     price: int = 0
     description: str = ""
-    pictures: list[str] = [""]
+    pictures: list = [""]
     auction_time: datetime = datetime.now()
     is_auction: int = 0
