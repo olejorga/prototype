e83c88b3e055e4ed03d575d8b892825d8f1d679d
simenyo <simen.90@gmail.com>
2021-10-20 22:24:42 +0200

    creating a new bid will now append correct itemid. Updating a bid no longer resets this itemid to 0

--- src/models/bid.py
+++ src/models/bid.py
@@ -5,3 +5,8 @@ class Bid(BaseModel):
     bid_value: int = 0
     item_id: int = 0
     user_id: int = 0
+
+
+class UpdateBid(BaseModel):
+    bid_value: int = None
+    user_id: int = None
--- src/routers/bids.py
+++ src/routers/bids.py
@@ -3,11 +3,10 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
-
 from pysondb import db
+from pysondb.db import DataNotFoundError
 
-
-from ..models.bid import Bid
+from ..models.bid import Bid, UpdateBid
 
 
 bid_db = db.getDb("data/bids.json")
@@ -17,7 +16,6 @@ templates = Jinja2Templates(directory="src/templates")
 router = APIRouter()
 
 
-# TODO get item id as a variable on each bid.
 @router.get("/bids/", tags=['bids view'], response_class=HTMLResponse)
 async def read_bids_view(request: Request):
     return templates.TemplateResponse("bids.html", {
@@ -27,6 +25,7 @@ async def read_bids_view(request: Request):
     })
 
 
+# item_id gets parsed into this after pressing "Kjøp"
 @router.get("/bids/{id}", tags=['bids view'], response_class=HTMLResponse)
 async def read_bid_view(bid_id: int, request: Request):
     return templates.TemplateResponse("bids.html", {
@@ -36,16 +35,24 @@ async def read_bid_view(bid_id: int, request: Request):
     })
 
 
-@router.post("/api/bids/", tags=['bids api'])
-async def create_bid(bid: Bid):
-    id = bid_db.add(jsonable_encoder(bid))
-    return id
+# include user_id from form in item.html in someway, then a bid can be set to both the item_id and user_id
+@router.post("/api/bids/{item_id}", tags=['bids api'])
+async def create_bid(bids_item_id: int, bid: Bid):
+    bid.item_id = bids_item_id
+#    bid.user_id = parameter
+    bid_id = bid_db.add(jsonable_encoder(bid))
+    return bid_id
 
 
-@router.put("/api/bids/{id}", tags=['bids api'])
-async def update_bid(id: int, bid: Bid):
-    bid_db.updateById(id, jsonable_encoder(bid))
-    return id
+@router.put("/api/bids/{bid_id}", tags=['bids api'])
+async def update_bid(bid_id: int, bid: UpdateBid):
+    if bid.bid_value is not None:
+        bid_db.updateById(bid_id, {"bid_value": bid.bid_value})
+
+    if bid.user_id is not None:
+        bid_db.updateById(bid_id, {"user_id": bid.user_id})
+
+    return bid_id
 
 
 @router.delete("/api/bids/{id}", tags=['bids api'])
