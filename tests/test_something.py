6ca496da2faf2aa463368856c121d8f4d3c3c596
Ole <36864120+olejorga@users.noreply.github.com>
2021-10-26 21:22:40 +0200

    fix: Temp fjernet test innhold

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,10 +1,5 @@
-from os import environ
 from fastapi.testclient import TestClient
 from ..src.app.main import app
-
-
-environ["LISTINGS_REPO_PATH"] = "tests/data/listings.dat"
-environ["USERS_REPO_PATH"] = "tests/data/users.dat"
 
 
 client = TestClient(app)
